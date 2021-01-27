9a7dc24fccdb032c42e23e5dd213a9c77904a5b3
kairosity <karinafinegan@gmail.com>
2021-01-27 20:13:25 +0000

    style: make the profile avatar & points username more responsive

--- static/js/script.js
+++ static/js/script.js
@@ -4,14 +4,15 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.tabs').tabs();
     $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
 
 
-    // not working yet
-     $('ul.right li').on('click', function(){
-                if($(this).hasClass('active')){
-                    $(this).removeClass('active');
-                }
-                $(this).addClass('active');
-            });
+    // // not working yet
+    //  $('ul.right li').on('click', function(){
+    //             if($(this).hasClass('active')){
+    //                 $(this).removeClass('active');
+    //             }
+    //             $(this).addClass('active');
+    //         });
 
 });
