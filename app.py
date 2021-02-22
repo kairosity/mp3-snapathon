77c7d95114272036ae73bd4460d3e123c211fdc4
kairosity <karinafinegan@gmail.com>
2021-02-22 14:03:34 +0000

    testing: test awards func and sheduler

--- app.py
+++ app.py
@@ -138,8 +138,8 @@ These are the scheduled functions:
 2. new_comp() runs automatically on Monday at 0:00AM
 '''
 scheduler = BackgroundScheduler()
-scheduler.add_job(awards, 'cron', day_of_week='sun',
-                  hour=22, minute=00, second=0,
+scheduler.add_job(awards, 'cron', day_of_week='mon',
+                  hour=14, minute=09, second=0,
                   start_date='2021-01-24 00:00:00')
 scheduler.add_job(new_comp, 'cron', [mongo], day_of_week='mon',
                   hour=00, minute=00, second=0,
