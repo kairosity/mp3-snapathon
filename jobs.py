a9ac79d32fa4f2523071573f1a73d8a7e86d2676
kairosity <karinafinegan@gmail.com>
2021-03-14 18:06:26 +0000

    deployed schedule test2

--- jobs.py
+++ jobs.py
@@ -8,13 +8,13 @@ These are the scheduled functions:
 '''
 scheduler = BackgroundScheduler()
 scheduler.add_job(awards, 'cron', day_of_week='sun',
-                  hour=22, minute=00, second=0,
+                  hour=22, minute=0, second=0,
                   start_date='2021-01-24 00:00:00')
 scheduler.add_job(new_comp, 'cron', [mongo], day_of_week='mon',
-                  hour=00, minute=00, second=0,
+                  hour=00, minute=0, second=0,
                   start_date='2021-01-24 00:00:00')
 scheduler.add_job(test_function, 'cron', [mongo], day_of_week='sun',
-                  hour=18, minute=05, second=0,
+                  hour=18, minute=9, second=0,
                   start_date='2021-01-24 00:00:00')
 
 scheduler.start()
