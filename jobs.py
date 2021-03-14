7bebf83eb5f3861742307ea7e1111672e70ea42b
kairosity <karinafinegan@gmail.com>
2021-03-14 19:15:09 +0000

    test: APScheduler in Heroku config tests6

--- jobs.py
+++ jobs.py
@@ -1,4 +1,4 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BlockingScheduler
 from app import awards, new_comp, test_function, mongo
 
 '''
@@ -6,13 +6,17 @@ These are the scheduled functions:
 1. awards() runs automatically on Sunday at 22:00PM
 2. new_comp() runs automatically on Monday at 0:00AM
 '''
-scheduler = BackgroundScheduler()
+scheduler = BlockingScheduler()
 scheduler.add_job(awards, 'cron', day_of_week='sun',
-                  hour=22, minute=0, second=0)
+                  hour=22, minute=0, second=0,
+                  start_date='2021-02-24')
 scheduler.add_job(new_comp, 'cron', [mongo], day_of_week='mon',
-                  hour=00, minute=0, second=0)
+                  hour=00, minute=0, second=0,
+                  start_date='2021-02-24')
 scheduler.add_job(test_function, 'cron', day_of_week='sun',
-                  hour=18, minute=40, second=0,
+                  hour=19, minute=19, second=0,
                   start_date='2021-02-24')
 
 scheduler.start()
+
+print("testing")
