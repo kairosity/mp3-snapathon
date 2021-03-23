c9701062bd9af767c0c993ac2793152b48f8ba03
kairosity <karinafinegan@gmail.com>
2021-03-23 16:24:57 +0000

    feat: testing security features with debug set to false

--- app.py
+++ app.py
@@ -873,4 +873,4 @@ def request_timeout(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
